f656ae67f526132439c47b15920715f28bfe5436
Adetokunbo Ajayi <adetokaj@gmail.com>
2024-12-04 12:20:48 +0000

    my first code

---
+++ src/demo1/SimpleArithmetic.java
@@ -0,0 +1,20 @@
+package demo1;
+
+public class SimpleArithmetic {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		int a = 5;
+		int b = 10;
+		int c = a + b;
+	System.out.println(c);
+		
+		
+		
+
+	}
+
+}
